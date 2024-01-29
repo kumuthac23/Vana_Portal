f93b2ad61f9878073c96d2bebf051bfb3f289933
Logeswaran <logeswaran2108@gmail.com>
2024-01-29 16:18:49 +0530

    on hover image changes

--- src/common/component/reusablecard/ReusableCard.tsx
+++ src/common/component/reusablecard/ReusableCard.tsx
@@ -1,6 +1,6 @@
 // CollectionPage.tsx
 
-import React from "react";
+import React, { useState } from "react";
 import { Collection} from "../../../interface/type"; 
 import { Link } from "react-router-dom";
 import { Container, Typography, Grid, Card, CardActionArea, CardContent, CardMedia, Divider } from "@mui/material";
@@ -10,6 +10,17 @@ interface Props {
 }
 
 const CollectionPage: React.FC<Props> = ({ collection }) => {
+
+  const [hoveredProductId, setHoveredProductId] = useState<string | null>(null);
+
+  const handleMouseEnter = (productId: string) => {
+    setHoveredProductId(productId);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredProductId(null);
+  };
+
   return (
     <Container sx={{marginTop:"10px"}}>
       <Typography variant="h4" align="center" gutterBottom>
@@ -19,12 +30,13 @@ const CollectionPage: React.FC<Props> = ({ collection }) => {
       <Grid container spacing={3}>
         {collection.products.map((product) => (
           <Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
-            <Card sx={{height: 400,width: 280,margin: "0 auto"}}>
+            <Card sx={{height: 400,width: 280,margin: "0 auto"}} onMouseEnter={() => handleMouseEnter(product._id)}
+              onMouseLeave={handleMouseLeave}>
               <CardActionArea component={Link} to={`/product/${product._id}`}>
                 <CardMedia
                   component="img"
                   height="200"
-                  image={product.image}
+                  image={hoveredProductId === product._id ? product.images[1] : product.images[0]}
                   alt={product.title}
                 />
                 <CardContent sx={{ height: 150, overflow: "hidden", textOverflow: "ellipsis" }}>
