878f5c70b1a5efa8dc54a1773b671635a782f195
dharaniya-thiyagarajan <dharaniya2000@gmail.com>
2024-01-31 11:39:16 +0530

    homepage banner image

--- src/pages/home/HomePageBanner.tsx
+++ src/pages/home/HomePageBanner.tsx
@@ -2,18 +2,17 @@ import { Box } from "@mui/material";
 
 function HomePageBanner() {
     return (
-     
-        <Box
-          sx={{
-            backgroundImage: "url('/public/images/homepagebanner.jpg')", // Replace '/path/to/your/image.jpg' with the path to your image file
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            height: "400px",
-            width: "100%",
-          }}
-        ></Box>
-      
+      <Box
+        sx={{
+          backgroundImage:
+            "url('/public/images/homepagebannerimage.JPG')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "100vh ",
+          width: "100%",
+        }}
+      ></Box>
     );
 }
 
