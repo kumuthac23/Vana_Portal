9fd4e67e4c1d2530bd8993709d1245f1dbe62306
dharaniya-thiyagarajan <dharaniya2000@gmail.com>
2024-02-01 20:39:19 +0530

    homepage banner  content

--- src/pages/home/HomePageBanner.tsx
+++ src/pages/home/HomePageBanner.tsx
@@ -1,18 +1,36 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 function HomePageBanner() {
   return (
-    <Box
-      sx={{
-    
-        backgroundImage: "url('/public/images/home.JPG')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        height: "400px",
-        width: "100%",
-      }}
-    ></Box>
+    <>
+      <Box sx={{ textAlign: "center", py: 2 }}>
+        <Typography
+          variant="h4"
+          sx={{
+            fontWeight: 600,
+            fontFamily: '"Crimson Text", serif',
+            fontStyle: "italic",
+          }}
+        >
+          Shine bright, just like a diamond's light.
+        </Typography>
+        <Typography variant="h6" sx={{ fontSize: "15px", py: 2 }}>
+          Shine bright, a beacon in the night, With brilliance that dazzles,
+          pure and white.
+        </Typography>
+      </Box>
+
+      <Box
+        sx={{
+          backgroundImage: "url('/public/images/2.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "90vh",
+          width: "100vw",
+        }}
+      ></Box>
+    </>
   );
 }
 
