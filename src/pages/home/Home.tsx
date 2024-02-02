687dcc0505a3c6af067835af4b0abc73794c712a
dharaniya-thiyagarajan <dharaniya2000@gmail.com>
2024-02-02 12:44:53 +0530

    New arrivals

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,21 @@
 import Box from "@mui/material/Box";
 import AboutByVana from "./AboutByVana";
 import HomePageBanner from "./HomePageBanner";
+import NewArriavals from "./NewArriavals";
 
 function Home() {
    
   return (
     <>
       <Box>
-        <HomePageBanner/>
+        <HomePageBanner />
+      </Box>
+      <Box
+        sx={{
+          mb: 2,
+        }}
+      >
+        <NewArriavals />
       </Box>
       <Box
         sx={{
--- src/pages/home/NewArriavals.tsx
+++ src/pages/home/NewArriavals.tsx
@@ -1,16 +1,45 @@
-import { Box} from '@mui/material'
-import {  Typography } from "@mui/material";
-import { useState } from 'react';
+import { Box, Grid } from "@mui/material";
+import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Product } from "../../interface/type";
+import CommonProductCard from "../../common/component/commonCard/CommonProductCard";
+// import { getNewArrivalProductsData } from "../../services/api";
 
 function NewArriavals() {
-    const [newArrivalProductsData, setNewArrivalProductsData] = useState<IProduct[]>([]);
+  const [newArrivalProductsData, setNewArrivalProductsData] = useState<
+    Product[]
+  >([]);
+  const [hoveredProductImage, setHoveredProductImage] = useState<string | null>(
+    null
+  );
+
+  const handleMouseEnter = (productId: string) => {
+    setHoveredProductImage(productId);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredProductImage(null);
+  };
+
+  // async function fetchData() {
+  //   try {
+  //     const newArrivalProducts = await getNewArrivalProductsData();
+  //     setNewArrivalProductsData(newArrivalProducts);
+
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
   return (
     <Box>
       <Box
-        display={"flex"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        pb={1}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
       >
         <Typography
           sx={{
@@ -24,14 +53,21 @@ function NewArriavals() {
         </Typography>
       </Box>
       <Box>
-        {newArrivalProductsData.map((product, index) => (
-          <Box key={index}>
-           
-          </Box>
-        ))}
+        <Grid>
+          {newArrivalProductsData.map((product) => (
+            <Grid item  xs={12} sm={6} md={4} lg={3}>
+              <CommonProductCard
+                product={product}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                hoveredProductImage={hoveredProductImage}
+              />
+            </Grid>
+          ))}
+        </Grid>
       </Box>
     </Box>
   );
 }
 
-export default NewArriavals
+export default NewArriavals;
